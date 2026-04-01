27a0d19e92fff0f5688e52f67dad0776836f8b69
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-04-01 17:43:48 +0530

    fixed:various things

--- next.config.js
+++ next.config.js
@@ -107,7 +107,6 @@ const nextConfig = {
   // Ensure MUI and other packages are transpiled so vendor chunks are generated correctly
   transpilePackages: ["@mui/material", "@mui/system", "@mui/utils"],
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "http",
